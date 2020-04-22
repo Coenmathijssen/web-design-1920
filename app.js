d60ac78f3c989c3b60b8304ab8b240520771248c
Coen Mathijssen <mathijssen.coen@gmail.com>
2020-04-22 11:44:08 +0200

    added subtitles and animations

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const app = express()
 const path = require('path')
 const socket = require('socket.io')
 
-// const server = app.listen(3000, '192.168.2.5', () => {
-//   console.log('listening on port: 3000')
-// })
-
-// Setting up port for express to use
-const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('listening on port: ', process.env.PORT)
+const server = app.listen(3000, '192.168.2.5', () => {
+  console.log('listening on port: 3000')
 })
+
+// // Setting up port for express to use
+// const server = app.listen(process.env.PORT || 3000, () => {
+//   console.log('listening on port: ', process.env.PORT)
+// })
 
 const io = socket(server)
 
@@ -23,8 +23,10 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
+  console.log('device connected')
   // Handle chat event
   socket.on('vibrate', () => {
+    console.log('joe')
     io.sockets.emit('vibrate')
   })
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,9 @@
+import { startSubtitles } from './subtitles.js'
+
 // Make connection
 const socket = io()
 
 const button = document.getElementById('play-button')
-
-function vibrate (ms) {
-  navigator.vibrate(ms)
-}
-
-// function vibratePattern () {
-//   console.log('starting')
-//   document.getElementById('play-button').textContent = 'playing'
-//   document.getElementById('video').play()
-//   window.setTimeout(() => {
-//     console.log('joe')
-//     window.navigator.vibrate(1500)
-//   }, 53500)
-
-//   window.setTimeout(() => {
-//     window.navigator.vibrate(500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500)
-//   }, 56000)
-  window.navigator.vibrate([0, 53500, 1500, 18000, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 56000, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500])
-// }
 
 // Emit events
 button.addEventListener('click', () => {
@@ -33,22 +16,20 @@ socket.on('vibrate', () => {
   console.log('starting')
   document.getElementById('play-button').textContent = 'playing'
   document.getElementById('video').play()
+  startSubtitles()
+
   window.setTimeout(() => {
-    console.log('joe')
+    console.log('jump scare')
     window.navigator.vibrate(1500)
-  }, 53500)
+  }, 203000)
 
   window.setTimeout(() => {
     console.log('alarm')
     window.navigator.vibrate([500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500])
-  }, 73000)
+  }, 223500)
 
   window.setTimeout(() => {
     console.log('alarm 2')
     window.navigator.vibrate([500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500, 500, 1500])
-  }, 163000)
+  }, 313000)
 })
-
-// button.addEventListener('click', vibratePattern)
-
-// buttonSec.addEventListener('click', vibratePattern)
